f4e0a19a1a929af9c711edf141079c898c7040fb
Fmafjrv103 <fmafjev103@outlook.com>
2025-02-24 23:13:37 -0700

    New File

--- main.c
+++ main.c
@@ -1,28 +1,20 @@
 #include "item.h"
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
-
-int main(int argc, char const *argv[])
-{
-    if (argc != 2)
-    {
-        printf("");
-    }
-    
-    return 0;
-}
 
 void add_item(Item *item_list, double price, char *sku, char *category, char *name, int index)
 {
-    Item *item_list;
-    item_list = (Item *)malloc(sizeof(Item));
-    item_list->category = (char *)malloc(sizeof(char));
-    item_list->category = *category;
-    item_list->name = (char *)malloc(sizeof(char));
-    item_list->name = *name;
-    item_list->sku = (char *)malloc(sizeof(char));
-    item_list->sku = *sku;
-    item_list->price = price; 
+    item_list[index].category = (char *)malloc(sizeof(char));
+    item_list[index].category = category;
+
+    item_list[index].name = (char *)malloc(sizeof(char));
+    item_list[index].name = name;
+
+    item_list[index].sku = (char *)malloc(sizeof(char));
+    item_list[index].sku = sku;
+
+    item_list[index].price = price; 
 }
 
 void free_items(Item *item_list, int size)
@@ -36,10 +28,51 @@ void free_items(Item *item_list, int size)
 double average_price(Item *item_list, int size)
 {
     double average = 0;
+    for (int i = 0; i < size; i++)
+    {
+        average += item_list[i].price;
+    }
+    average /= size;
+    
     return average;
 }
 
 void print_items(Item *item_list, int size)
 {
+    for (int i = 0; i < size; i++)
+    {
+        printf("*******************************\n");
+        printf("            Item %d            \n", i+1);
+        printf("*******************************\n");
+        printf("Name: %s", item_list[i].name);
+        printf("Price: %f", item_list[i].price);
+        printf("Catagory: %s", item_list[i].category);
+        printf("SKU: %s", item_list[i].sku);
+    }
+}
 
+int main(int argc, char const *argv[])
+{
+    if (argc != 2)
+    {
+        printf("usage: %s<size>", argv[1]);
+    }
+
+    char *searchSku = argv[1];
+
+    int size = 5;
+    Item *itemList;
+
+    itemList = (Item *)malloc(sizeof(Item) * size);
+
+    if(size < 0) 
+    {
+        printf("size to small");
+    }
+    
+    add_item(&itemList[size], 5, "19282", "breakfast", "reese\'s cereal", 0);
+    print_items(&itemList[size], 1);
+    free_items(&itemList[size], size);
+
+    return 0;
 }
